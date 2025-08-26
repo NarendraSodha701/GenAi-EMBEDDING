da8793bca102eea4484569db53e7c1569b8fe569
Ns <narendra.singh434@outlook.com>
2025-08-26 16:40:21 +0530

    commit

--- src/main/java/com/embedding/ai/AiembeddingConfig.java
+++ src/main/java/com/embedding/ai/AiembeddingConfig.java
@@ -1,14 +1,21 @@
 package com.embedding.ai;
 
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.azure.AzureOpenAiChatModel;
 import dev.langchain4j.model.azure.AzureOpenAiEmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.qdrant.QdrantEmbeddingStore;
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.QdrantGrpcClient;
+import io.qdrant.client.grpc.Collections;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutionException;
 
 
 @Configuration
@@ -40,6 +47,9 @@ public class AiembeddingConfig {
     @Value("${langchain4j.qdrant.key}")
     private String qdrantKey;
 
+    private static final Collections.Distance distance = Collections.Distance.Cosine;
+
+
     @Bean
     public AzureOpenAiEmbeddingModel embeddingModel() {
         return AzureOpenAiEmbeddingModel.builder()
@@ -62,16 +72,37 @@ public class AiembeddingConfig {
     @Bean
     public QdrantEmbeddingStore qdrantEmbeddingStore() {
         return QdrantEmbeddingStore.builder()
-                .collectionName("restaurant_menu")
+                .collectionName("ai_hotel_menu")
                 .host(qdrantUrl)
                 .port(6334)
                 .apiKey(qdrantKey)
-                .useTls(true)
+                .useTls(false)
                 .build();
     }
 
     @Bean
-    public ContentRetriever contentRetriever(EmbeddingModel embeddingModel, QdrantEmbeddingStore embeddingStore) {
+    public QdrantClient qdrantConfig() throws ExecutionException, InterruptedException {
+        QdrantClient client = new QdrantClient(
+                QdrantGrpcClient.newBuilder(qdrantUrl, 6334, false).withApiKey(qdrantKey)
+                        .build());
+        if (!client.collectionExistsAsync("ai_hotel_menu").get()) {
+            Collections.CollectionOperationResponse response = client.createCollectionAsync(
+                    "ai_hotel_menu",
+                    Collections.VectorParams.newBuilder().setDistance(distance).setSize(768).build()).get();
+        }
+        return client;
+    }
+
+    @Bean
+    public ContentRetriever contentRetriever(EmbeddingModel embeddingModel) {
+        EmbeddingStore<TextSegment> embeddingStore =
+                QdrantEmbeddingStore.builder()
+                        .collectionName("ai_hotel_menu")
+                        .host(qdrantUrl)
+                        .port(6334)
+                        .apiKey(qdrantKey)
+                        .useTls(false)
+                        .build();
         return EmbeddingStoreContentRetriever.builder()
                 .embeddingStore(embeddingStore)
                 .embeddingModel(embeddingModel)
